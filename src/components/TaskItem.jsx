bc255e317f37b775c7ae1a59e221a36add689d97
Yuy-1 <DM-Yuy@proton.me>
2022-12-31 17:03:05 -0500

    fixed setIsChecked value

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -11,7 +11,7 @@ const TaskItem = ({task}) => {
   const [isChecked, setIsChecked] = useState(task.checked);
 
   const handleCheckboxChange = (e) =>{
-    setIsChecked(!)
+    setIsChecked(!isChecked);
   }
 
   return (
