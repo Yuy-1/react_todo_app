9aff7d91fc3801c08330194a5599cb806868afa9
Yuy-1 <DM-Yuy@proton.me>
2022-12-31 18:35:22 -0500

    Copy from CustomForm change state to updatedTaskName, setUpdatedTaskName switch to CheckIcon

---
+++ src/components/EditForm.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+//library imports
+import { CheckIcon } from "@heroicons/react/24/outline/index"
+
+const EditForm = ({ editedTask }) => {
+  const [updatedTaskName, setUpdatedTaskName] = useState(editedTask.name);
+
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  return (
+    <div
+      role="dialog"
+      aria-labelledby="editTask"
+      // onClick={}
+
+    >
+    <form
+      className="todo"
+      onSubmit={handleFormSubmit}
+    >
+      <div className="wrapper">
+        <input
+          type="text"
+          id="editTask"
+          className="input"
+          value={updatedTaskName}
+          onInput={(e) => setUpdatedTaskName(e.target.value)}
+          required
+          autoFocus
+          maxLength={60}
+          placeholder="Update Task"
+        />
+        <label htmlFor="editTask"
+        className="label"
+        >Update Task</label>
+      </div>
+      <button
+        className="btn"
+        aria-label={`Confirm edited task to now read ${updatedTaskName}`}
+        type="submit"
+      >
+      <CheckIcon strokeWidth={2} height={24} width={24}/>
+      </button>
+    </form>
+    </div>
+  )
+}
+
+export default EditForm
