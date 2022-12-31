bec7d5a939db97ede19ba57e89024b54ca30b60a
Yuy-1 <DM-Yuy@proton.me>
2022-12-31 16:45:56 -0500

    add checkmark next to task item add css styles to input and task items

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -6,6 +6,24 @@ import styles from "./TaskItem.module.css"
 const TaskItem = ({task}) => {
   return (
     <li className={styles.task}>
+      <div className={styles["task-group"]}>
+        <input
+          type="checkbox"
+          checked={task.checked}
+          //onChange={}
+          name={task.name}
+          id={task.id}
+        />
+        <label
+          htmlFor={task.id}
+          className={styles.label}
+        >
+          {task.name}
+          <p className={styles.checkmark}>
+
+          </p>
+        </label>
+      </div>
 
     </li>
   )
