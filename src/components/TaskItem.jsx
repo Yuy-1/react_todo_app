cb1491a81ecadcf9942e3be9f31711755b1f7a2b
Yuy-1 <DM-Yuy@proton.me>
2022-12-31 17:46:25 -0500

    fix typo

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -39,7 +39,7 @@ const TaskItem = ({task}) => {
       <div className={styles["task-group"]}>
         <button
           className="btn"
-          aria-label={`Update ${task.name} Task``}
+          aria-label={`Update ${task.name} Task`}
           //onClick={}
         >
           <PencilSquareIcon width={24} height={24} />
@@ -48,7 +48,6 @@ const TaskItem = ({task}) => {
         <button
           className={`btn ${styles.delete}`}
           aria-label={`Delete ${task.name} Task`}
-          //onClick={}
         >
           <TrashIcon width={24} height={24} />
         </button>
