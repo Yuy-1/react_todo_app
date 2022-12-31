de4bcca150450c21b5e93c7bab973a0689022360
Yuy-1 <DM-Yuy@proton.me>
2022-12-31 14:34:58 -0500

    import hero icons add plus icon to form add My Task List add classes to form wrapper div

--- src/components/CustomForm.jsx
+++ src/components/CustomForm.jsx
@@ -1,3 +1,5 @@
+import { PlusIcon } from "@heroicons/react/20/solid/index.js";
+
 const CustomForm = () => {
 
   const handleFormSubmit = (e) => {
@@ -12,9 +14,28 @@ const CustomForm = () => {
       onSubmit={handleFormSubmit}
     >
       <div className="wrapper">
-
+        <input
+          type="text"
+          id="task"
+          className="input"
+          //value={task}
+          //onInput={(e) => setTask(e.target.value)}
+          required
+          autoFocus
+          maxLength={60}
+          placeholder="Enter Task"
+        />
+        <label htmlFor="task"
+        className="label"
+        >Enter Task</label>
       </div>
-
+      <div
+        className="btn"
+        aria-label="Add Task"
+        type="submit"
+      >
+      <PlusIcon />
+      </div>
     </form>
   )
 }
